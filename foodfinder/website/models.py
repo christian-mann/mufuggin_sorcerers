d936d05872e3bc800e6661aaf7a64c59c00f9b46
Christian Mann <christian-mann@utulsa.edu>
2013-11-09 17:48:39 -0600

    Added Event model to models

--- foodfinder/website/models.py
+++ foodfinder/website/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Event(models.Model):
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    location = models.CharField(max_length=1000)
+    notes = models.TextField()
+
+    creation_time = models.DateTimeField(auto_now_add=True)
+
+    @property
+    def duration(self):
+        return end_time - start_time
