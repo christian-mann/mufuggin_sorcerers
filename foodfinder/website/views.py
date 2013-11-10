bebe0ecaa860e366eb6c46cd923674e44eae88ce
Christian Mann <christian-mann@utulsa.edu>
2013-11-09 19:38:10 -0600

    Added event management facilities

--- foodfinder/website/views.py
+++ foodfinder/website/views.py
@@ -1,5 +1,8 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
 from django.shortcuts import render_to_response
+
+from website.models import Event, EventForm
 
 # Create your views here.
 
@@ -9,3 +12,21 @@ def home(request):
         'events': events
     })
 
+def manage_event(request, event_id=None):
+    if request.method == 'POST':
+        form = EventForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # any other hooks we want to put here
+            return redirect('home')
+
+    elif event_id:
+        e = Event.objects.filter(pk=event_id)[0]
+        if e:
+            form = EventForm(instance=e)
+    else:
+        form = EventForm()
+
+    return render(request, 'manage_event.html', {
+        'form': form
+    })
