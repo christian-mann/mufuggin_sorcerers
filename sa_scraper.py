2d058f54ee4aa7778767d4f9e3ce959fa20b7334
labels <leland-abels@utulsa.edu>
2013-11-09 21:17:36 -0600

    Scraper Initial Working version
    outputs to events.pkl

--- sa_scraper.py
+++ sa_scraper.py
@@ -1,13 +1,42 @@
 import json
 import re
 import urllib2
+import pickle
 
 class sa_scraper:
 	def scrape(self):
 		sa_data = urllib2.urlopen("https://www.googleapis.com/calendar/v3/calendars/utulsa.edu_phngeno8i8nb54u70nepv793m0%40group.calendar.google.com/events?orderBy=startTime&singleEvents=true&timeZone=America%2FChicago&key=AIzaSyBYMs84E3yw0NhKiC8i24Xifukh8du8RrM")
 		sa_data = sa_data.read()
 		sa_data = json.loads(sa_data)
-		print sa_data
+		events = sa_data['items']
+		events_simple = []
+		for event in events:
+			new_event = {'summary':'','start':'','end':'','description':''}
+			if 'summary' in event.keys():
+				new_event['summary'] = event['summary'].encode('utf-8')
+			if 'start' in event.keys():		
+				if 'date' in event['start'].keys():
+					new_event['start'] = event['start']['date'].encode('utf-8')
+				if 'dateTime' in event['start'].keys():
+					new_event['start'] = event['start']['dateTime'].encode('utf-8')
+			if 'end' in event.keys():			
+				if 'date' in event['end'].keys():
+					new_event['end'] = event['end']['date'].encode('utf-8')
+				if 'dateTime' in event['end'].keys():
+					new_event['end']  = event['end']['dateTime'].encode('utf-8')
+			if 'description' in event.keys():
+				new_event['description'] = event['description'].encode('utf-8')
+			if 'location' in event.keys():
+				new_event['location'] = event['location'].encode('utf-8')
+			
+			events_simple.append(new_event)
+		#end loop	
+		hits = ('free','food','lunch')
+		for event in events_simple:
+			event['food'] = any(hit in event['description'] for hit in hits)
+				
+		with  open('events.pkl','w') as output:
+			pickle.dump(events_simple, output)
 
 scrape = sa_scraper()
 scrape.scrape()
