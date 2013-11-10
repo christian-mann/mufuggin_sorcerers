9c8111ff4c94e61e899548398b04834cb2527408
labels <leland-abels@utulsa.edu>
2013-11-10 05:03:21 -0600

    Updating time limits
    lunch from before 2009 is probably expired by now.

--- sa_scraper.py
+++ sa_scraper.py
@@ -6,7 +6,7 @@ from datetime import datetime
 
 class sa_scraper:
 	def scrape(self):
-		sa_data = urllib2.urlopen("https://www.googleapis.com/calendar/v3/calendars/utulsa.edu_phngeno8i8nb54u70nepv793m0%40group.calendar.google.com/events?orderBy=startTime&singleEvents=true&timeZone=America%2FChicago&key=AIzaSyBYMs84E3yw0NhKiC8i24Xifukh8du8RrM")
+		sa_data = urllib2.urlopen("https://www.googleapis.com/calendar/v3/calendars/utulsa.edu_phngeno8i8nb54u70nepv793m0%40group.calendar.google.com/events?orderBy=startTime&maxResults=500&timeMin=2013-01-01T00%3A00%3A00-05%3A00&singleEvents=true&timeZone=America%2FChicago&key=AIzaSyBYMs84E3yw0NhKiC8i24Xifukh8du8RrM")
 		sa_data = sa_data.read()
 		sa_data = json.loads(sa_data)
 		events = sa_data['items']
