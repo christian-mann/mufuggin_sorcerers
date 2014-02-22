bb2283d4a575345bf43f17b01700244c5b9c8df8
Christian Mann <christian-mann@utulsa.edu>
2014-02-22 16:23:18 -0600

    Changed max_length to 200

--- foodfinder/website/models.py
+++ foodfinder/website/models.py
@@ -6,7 +6,7 @@ from website import maps
 # Create your models here.
 
 class Event(models.Model):
-    title = models.CharField()
+    title = models.CharField(max_length=200)
     start_time = models.DateTimeField()
     end_time = models.DateTimeField()
     location = models.CharField(max_length=1000)
